9a6f87ddc3504ac2f554cde122feb58b22d81443
GhostShdow <KZ@zhangkundemacbook-pro.local>
2016-04-13 14:44:09 +0800

    Initial Commit

---
+++ MultipleAlbums/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  MultipleAlbums
+//
+//  Created by 张坤 on 16/4/13.
+//  Copyright © 2016年 KZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ MultipleAlbums/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MultipleAlbums
+//
+//  Created by 张坤 on 16/4/13.
+//  Copyright © 2016年 KZ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
